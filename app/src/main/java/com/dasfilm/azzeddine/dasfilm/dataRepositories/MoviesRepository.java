53834fb97a6b0f1a94c2b25dd8e6ba5ba47a17ed
azeddine <chenineazeddine5@gmail.com>
2018-03-28 15:55:55 +0100

    implmenting the singleton pattern

--- app/src/main/java/com/dasfilm/azzeddine/dasfilm/dataRepositories/MoviesRepository.java
+++ app/src/main/java/com/dasfilm/azzeddine/dasfilm/dataRepositories/MoviesRepository.java
@@ -1,8 +1,18 @@
 package com.dasfilm.azzeddine.dasfilm.dataRepositories;
 
+import com.dasfilm.azzeddine.dasfilm.APIUtils.ServiceGenerator;
+import com.dasfilm.azzeddine.dasfilm.dataModels.TMDBWebService;
+
 /**
  * Created by azeddine on 3/27/18.
  */
 
 public class MoviesRepository {
+    private TMDBWebService tmdbWebService = ServiceGenerator.createService(TMDBWebService.class);
+
+    private static MoviesRepository moviesRepositoryInstance = new MoviesRepository();
+    private MoviesRepository() {}
+
+
+
 }
